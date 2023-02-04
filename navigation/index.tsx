f9502dfd7843093b6ac2619e0eaa623efecb309f
Vickie Do <dovh@uci.edu>
2023-02-04 02:26:32 -0800

    set up template

---
+++ navigation/index.tsx
@@ -0,0 +1,82 @@
+/**
+ * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer, DefaultTheme, DarkTheme, useNavigation } from '@react-navigation/native';
+import { ColorSchemeName} from 'react-native';
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+import LinkingConfiguration from './LinkingConfiguration';
+
+import Profile from '../screens/Profile';
+import Questions from '../screens/Questions';
+import Feed from '../screens/Feed';
+
+
+export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  return (
+    <NavigationContainer
+      linking={LinkingConfiguration}
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+/**
+ * A root stack navigator is often used for displaying modals on top of all other content.
+ * https://reactnavigation.org/docs/modal
+ */
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+
+  return (
+    <Stack.Navigator
+    screenOptions={{ animation: 'fade' }}>
+      <Stack.Screen 
+          name="Root" 
+          component={BottomTabNavigator} 
+          options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
+
+/**
+ * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
+ * https://reactnavigation.org/docs/bottom-tab-navigator
+ */
+const BottomTab = createBottomTabNavigator<RootTabParamList>();
+
+function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Enter"
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme].tint,
+        tabBarStyle: { height: 100,},
+      }}>
+      <BottomTab.Screen
+        name="Feed"
+        component={Feed}
+      />
+      <BottomTab.Screen
+        name="Questions"
+        component={Questions}
+      />
+       <BottomTab.Screen
+          name="Profile"
+          component={Profile}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+
---
+++ screens/Questions.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Questions = () => {
+
+
+  return (
+    <View>
+      <Text>Questions</Text>
+    </View>
+  )
+}
+
+export default Questions
+
